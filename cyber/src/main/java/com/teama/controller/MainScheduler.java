f8b07233aad6407ebc835261835cd69b477fd5c3
WooJongSeong <whdtud@gmail.com>
2021-10-12 10:07:48 +0900

    [#17 책 대여, 예약, 반납 기능 추가] 자동 반납 기능 추가를 위해 스케쥴러 생성

---
+++ cyber/src/main/java/com/teama/controller/MainScheduler.java
@@ -0,0 +1,22 @@
+package com.teama.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.teama.loan.service.LoanService;
+
+@Component
+public class MainScheduler {
+
+	@Resource(name="loanService")
+	private LoanService loanService;
+	
+	@Scheduled(cron="* */1 * * * *")
+	public void autoLoan() {
+		System.out.println("scheduled-autoLoan");
+		
+		//loanService.autoLoan();
+	}
+}
